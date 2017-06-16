edfe3afcb4baf3103ebf3d0f13e625052cf11567
PixelZero <joris@lambooy.nl>
2017-06-16 16:18:55 +0200

    Shader fix. Phong Shading Model almost completely implemented

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -14,7 +14,7 @@ uniform vec4 ambient_Color_L1;
 uniform vec4 diffuse_Color_L1;
 uniform vec4 speculr_Color_L1;
 
-int alpha = 1;
+int alpha = 2;
 
 // shader output
 out vec4 outputColor;
@@ -26,16 +26,17 @@ void main()
     outputColor = texture( pixels, uv );// + 0.5f * vec4( normal.xyz, 1 );
 
 	// times the diffuse illumination (and the diffuse light color)
-	vec4 lightD1 = lightPos1 - position;
-	float nDotL = dot (normal, normalize(lightD1) );
+	vec4 lightD1 = normalize(lightPos1 - position);
+	float nDotL = dot (normal, lightD1 );
 	outputColor *= (nDotL * diffuse_Color_L1);
 
 	// plus the specular illumination
 	vec4 normalizedP = -normalize(position);
-	float nDotP = dot(normal, normalizedP);
+	float nDotP = max(0, dot(normal, normalizedP));
 	vec4 reflectedRay = normalizedP - 2 * nDotP * normal; // ?
-	float specularIntensity = pow(dot(reflectedRay, lightD1), alpha);
-	specularIntensity = max(0, -specularIntensity);
+	float dotProduct = min(1, max(0, -dot(reflectedRay, lightD1)));
+	float specularIntensity = pow(dotProduct, alpha);
+	//specularIntensity = 
 
 	outputColor += specularIntensity * speculr_Color_L1;
 	//outputColor.y = -specularIntensity / 10;// * speculr_Color_L1.y;
@@ -44,6 +45,6 @@ void main()
 	outputColor += ambient_Color_L1;
 
 	//outputColor.x = -nDotP;
-	//outputColor.y = -nDotP;
+	//outputColor.y = -specularIntensity;
 	//outputColor.z = nDotP;
 }
